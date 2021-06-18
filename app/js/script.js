06886457c11333dcb914d9604ed87d6fabb2c518
Pablo <arayap812@gmail.com>
2021-06-17 22:34:00 -0300

    mobile menu DONE!

--- app/js/script.js
+++ app/js/script.js
@@ -1,15 +1,23 @@
 const btnHamburger = document.querySelector('#btnHamburger')
+const body = document.querySelector('body')
 const header = document.querySelector('.header')
 const overlay = document.querySelector('.overlay')
+const fadeElems = document.querySelectorAll('.has-fade')
 
 btnHamburger.addEventListener('click', function(){
     if(header.classList.contains('open')){ //close menu
+        body.classList.remove('noscroll')
         header.classList.remove('open')
-        overlay .classList.remove('fade-in')
-        overlay.classList.add('fade-out')
-    } else {                                //open menu
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-in')
+            element.classList.add('fade-out')
+        })
+    } else {             
+        body.classList.add('noscroll')                   //open menu
         header.classList.add('open')
-        overlay.classList.remove('fade-out')
-        overlay.classList.add('fade-in')
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-out')
+            element.classList.add('fade-in')
+        })
     }
 })
